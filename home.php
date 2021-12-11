6860c6a0a74828e56491b0749d80e6eae67f950b
Lukas <75485295+1ukas31@users.noreply.github.com>
2021-12-11 15:45:13 +0100

    Welcome page
    Willkommensseite hinzugefügt.
    Video fehlt aber noch!!!

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+include "./html/head.html";
+include "./html/header.html";
+include "./html/footer.html";
+include "./html/foot.html";
+
+ ?>
+
+
+<div id="home">
+<br>
+<h1>Herzlich Willkommen!</h1>
+<h2>LUDOMO - Der Shop für alle Games</h1>
+<button type="button" class="button">Starte in das Gaminguniverse</button>
+</div>
