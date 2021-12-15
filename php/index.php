8cbdfe733d7200bb9b6f2a4a0722318e6d9e05cc
Lukas <75485295+1ukas31@users.noreply.github.com>
2021-12-15 17:08:59 +0100

    #

--- php/index.php
+++ php/index.php
@@ -1,8 +1,12 @@
 <?php
 
+//try catch bitte erledigen!!
+//include "db.php";
 include "../html/head.html";
 include "../html/header.html";
-include "../html/konto.html";
+include "../html/home.html";
 include "../html/footer.html";
 include "../html/foot.html";
+
+
 ?>
