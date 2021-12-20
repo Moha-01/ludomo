66e8438af6ff8ff5265837a90333918cd8e88cc4
Domilolblob <domi.merkle@web.de>
2021-12-20 23:26:23 +0100

    .

--- php/db.php
+++ php/db.php
@@ -11,7 +11,7 @@ $abfrage->execute();
 $verbindung=null;
 $server = 'mysql:host=localhost;dbname=ludomo';
 $verbindung = new PDO($server, "root", "",);
-$sqlBefehl = 'CREATE TABLE IF NOT EXISTS products(ID INTEGER AUTO_INCREMENT, brand_id VARCHAR (50), genre_name VARCHAR (50), S_M_id INTEGER, name VARCHAR (100), preis float, rabatt INTEGER, endpreis float,background varchar(200), discription varchar(200), picture varchar(200), fsk INTEGER, PRIMARY KEY (ID));';
+$sqlBefehl = 'CREATE TABLE IF NOT EXISTS products(ID INTEGER AUTO_INCREMENT, brand_id VARCHAR (50), genre_name VARCHAR (50), S_M_id INTEGER, name VARCHAR (100), preis float, rabatt INTEGER, endpreis float,background text, discription text, picture text, fsk INTEGER, PRIMARY KEY (ID));';
 $abfrage = $verbindung->prepare($sqlBefehl);
 $abfrage->execute();
 $sqlBefehl = 'CREATE TABLE IF NOT EXISTS user_tabelle(ID INTEGER AUTO_INCREMENT, username varchar(100), password varchar(100), email varchar(100), privalage int, PRIMARY KEY (ID));';
