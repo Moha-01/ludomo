6005a9b4e403640096be4d05685a06d2b7cc8c8d
Mohamed Haji <49024724+Moha-01@users.noreply.github.com>
2021-12-20 14:41:25 +0100

    Shop Filter Funktion und TestDaten erfolgte gut. Design noch fehlend.

--- php/shop.php
+++ php/shop.php
@@ -4,8 +4,6 @@ include "../html/head.html";
 include "../html/othersCss.html";
 include "../html/header.html";
 include "../html/shop.html";
-
-echo "test";
 include "../html/footer.html";
 include "../html/foot.html";
 
