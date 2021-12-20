e8f1de2c1e3a1b09378f1f00805ffd325789ad3e
Domilolblob <domi.merkle@web.de>
2021-12-20 23:42:22 +0100

    .

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+//try catch bitte erledigen!!
+include "db.php";
+include "../html/head.html";
+include "../html/header.html";
+include "../html/home.html";
+include "../html/footer.html";
+include "../html/foot.html";
+
+
+
+
+?>
