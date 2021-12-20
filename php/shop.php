daac03ee3528247fc925d0868c656132538ab6dd
Mohamed Haji <49024724+Moha-01@users.noreply.github.com>
2021-12-20 12:00:07 +0100

    anpassungen hinzugefügt

--- php/shop.php
+++ php/shop.php
@@ -1,1 +1,12 @@
-test
+<?php
+
+include "../html/head.html";
+include "../html/othersCss.html";
+include "../html/header.html";
+include "../html/shop.html";
+
+echo "test";
+include "../html/footer.html";
+include "../html/foot.html";
+
+?>
