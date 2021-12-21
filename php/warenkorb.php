367fcbfb0a70d1e41b52b529761e3523b15a4de4
Domilolblob <domi.merkle@web.de>
2021-12-21 20:26:14 +0100

    .

---
+++ php/warenkorb.php
@@ -0,0 +1,14 @@
+<?php
+include "../html/head.html";
+include "../html/othersCss.html";
+include "../html/header.html";
+include "../html/warenkorb.html";
+include "../html/footer.html";
+include "../html/foot.html";
+
+//$Einkaufswagen_Tab = unserialize($_COOKIE['Wahrenkorb']);
+
+
+
+
+?>
