391981f3a19c15652eeff596e62c8551e8eda867
Domilolblob <domi.merkle@web.de>
2021-12-22 02:15:10 +0100

    Impressum u. Datenschutz

---
+++ php/datenschutz.php
@@ -0,0 +1,7 @@
+<?php
+include "../html/head.html";
+include "../html/header.html";
+include "../html/datenschutz.html";
+include "../html/footer.html";
+include "../html/foot.html";
+?>
--- php/impressum.php
+++ php/impressum.php
@@ -1,1 +1,7 @@
-test
+<?php
+include "../html/head.html";
+include "../html/header.html";
+include "../html/impressum.html";
+include "../html/footer.html";
+include "../html/foot.html";
+?>
