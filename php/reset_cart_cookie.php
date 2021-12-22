5e3daf54ed0b5d7fa55e7b0dc63da39a83995670
Mohamed Haji <49024724+Moha-01@users.noreply.github.com>
2021-12-22 17:27:25 +0100

    +

--- php/reset_cart_cookie.php
+++ php/reset_cart_cookie.php
@@ -2,8 +2,7 @@
 function showwindow(){
   setcookie("Warenkorb","",time()-3600, "/");
   $message = "Your order has been received";
-  echo "<script type='text/javascript'>alert('$message');</script>";
+  echo "<script type='text/javascript'>alert('$message'); window.location=\"/ludomo/php/warenkorb.php\";</script>";
 }
 showwindow();
-include 'shop.php';
 ?>
