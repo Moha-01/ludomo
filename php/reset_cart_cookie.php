99e372d388a7a534e11cb01f4841fa278171262f
Domilolblob <domi.merkle@web.de>
2021-12-23 14:34:52 +0100

    .

--- php/reset_cart_cookie.php
+++ php/reset_cart_cookie.php
@@ -1,5 +1,6 @@
 <?php
 function showwindow(){
+// Der Einkauf wird abgeschlossen und der Einkaufswagen wird gelehrt 
   setcookie("Warenkorb","",time()-3600, "/");
   $message = "Your order has been received";
   echo "<script type='text/javascript'>alert('$message'); window.location=\"/dhbw/ludomo/php/warenkorb.php\";</script>";
